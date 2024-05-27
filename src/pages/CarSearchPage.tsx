a78a29305ad9fa62355fe7398fbe397fe157e9ee
ahsansaeed202 <ahsansaeed7t@gmail.com>
2024-05-28 01:47:29 +0500

    create Breadcrum page

--- src/pages/CarSearchPage.tsx
+++ src/pages/CarSearchPage.tsx
@@ -3,6 +3,7 @@ import styles from "../Assets/css/Pages.module.css";
 import CabSearch from "../components/CarSearch/CabSearch";
 import DateArea from "../components/CarSearch/DateArea";
 import Loader from "../components/Common/Loader";
+import Breadcrum from "../components/Common/Breadcrum";
 
 const CarSearchPage: FunctionComponent = () => {
   const [loading, setLoading]=useState(false)
@@ -10,7 +11,9 @@ const CarSearchPage: FunctionComponent = () => {
   if(loading) return <Loader/>
   return (
     <div className={styles.carSearch}>
+      <Breadcrum/>
       <CabSearch />
+
       <DateArea setLoading={setLoading} />
     </div>
   );
