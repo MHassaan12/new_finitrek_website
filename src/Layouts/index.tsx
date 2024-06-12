787aa0fe4f87e5ba59e9a32f2d22d41d7e5f9ba1
ahsansaeed202 <ahsansaeed7t@gmail.com>
2024-06-13 00:51:48 +0500

    f

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -1,7 +1,8 @@
 import { FunctionComponent } from "react"
-import Header from "../components/Common/Header"
+// import Header from "../components/Common/Header"
 import Footer from "../components/Common/Footer"
 import { Outlet } from "react-router-dom"
+import Header from "../components/LandingPage/Header"
 
 
 const Layout: FunctionComponent = () => {
